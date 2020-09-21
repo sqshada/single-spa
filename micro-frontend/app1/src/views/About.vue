b0b96d3462a10b8c614ce27b65ef080c64d4a3eb
chenle <chenle@forchange.tech>
2020-09-21 15:09:13 +0800

    feat: delete demo

--- micro-frontend/app1/src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>app1 about page</h1>
-  </div>
-</template>
--- micro-frontend/app2/src/views/Home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="home">
-    <h1>app2 home page</h1>
-  </div>
-</template>
